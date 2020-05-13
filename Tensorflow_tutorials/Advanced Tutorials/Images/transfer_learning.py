253041e3a23a24603b8a9bfb6f721fc7456a665e
lgcharpe <lgcharpe@gmail.com>
2020-05-13 12:07:42 +0200

    transfering

--- Tensorflow_tutorials/Advanced Tutorials/Images/transfer_learning.py
+++ Tensorflow_tutorials/Advanced Tutorials/Images/transfer_learning.py
@@ -19,3 +19,57 @@ tfds.disable_progress_bar()
 print(raw_train)
 print(raw_validation)
 print(raw_test)
+
+get_label_name = metadata.features['label'].int2str
+
+for image, label in raw_train.take(2):
+    plt.figure()
+    plt.imshow(image)
+    plt.title(get_label_name(label))
+
+# Format the data
+
+IMG_SIZE = 160  # All images will be resized to 160x160
+
+
+def format_example(image, label):
+    image = tf.cast(image, tf.float32)
+    image = (image/127.5) - 1
+    image = tf.image.resize(image, (IMG_SIZE, IMG_SIZE))
+    return image, label
+
+
+train = raw_train.map(format_example)
+validation = raw_validation.map(format_example)
+test = raw_test.map(format_example)
+
+BATCH_SIZE = 32
+SHUFFLE_BUFFER_SIZE = 1000
+
+train_batches = train.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
+validation_batches = validation.batch(BATCH_SIZE)
+test_batches = test.batch(BATCH_SIZE)
+
+for image_batch, label_batch in train_batches.take(1):
+    pass
+
+image_batch.shape
+
+IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
+
+# Create the base model from the pre-trained model MobileNet V2
+base_model = tf.keras.applications.MobileNetV2(
+    input_shape=IMG_SHAPE,
+    include_top=False,
+    weights='imagenet'
+)
+
+feature_batch = base_model(image_batch)
+print(feature_batch.shape)
+
+# This will prevent the weights from changing during training
+base_model.trainable = False
+
+# Let us take a look as the base model architecture
+print(base_model.summary())
+
